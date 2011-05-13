322144e718ad24d1f67fcec0f4496ccfa56143f6
M@ McCray <darthapo@gmail.com>
2011-05-12 22:05:55 -0500

    Initial import of rail skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskthis2::Application.config.secret_token = '30a50671dc183d4a832b644720df4371ad27a919676eb997d588eb8c908957d2e2c5c7ac6f1f01b7e9728125e8b6efaa567f0e1107814d19bdb05f58e0ac80a6'
